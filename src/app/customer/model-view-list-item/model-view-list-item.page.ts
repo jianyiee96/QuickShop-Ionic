27597f8375ee98d32819714195717e870006d019
Tan Wee Keat <tanwk_97@hotmail.com>
2020-05-10 08:18:56 +0800

    fixing ui

--- src/app/customer/model-view-list-item/model-view-list-item.page.ts
+++ src/app/customer/model-view-list-item/model-view-list-item.page.ts
@@ -11,9 +11,9 @@ import { SessionService } from 'src/app/session.service';
 })
 export class ModelViewListItemPage implements OnInit {
 
-
   public currentItem: Item;
   public resourcePath: string;
+  public imagePath: string;
 
   shoppingList: Item[];
 
@@ -22,83 +22,67 @@ export class ModelViewListItemPage implements OnInit {
     private navParams: NavParams,
     private sessionService: SessionService) {
 
-
     this.currentItem = navParams.get('input');
-    this.resourcePath = sessionService.getImageResourcePath();
+    this.resourcePath = sessionService.getImageResourcePath() + "Maps/";
     this.shoppingList = sessionService.getCurrentShoppingList();
 
     this.shoppingList.forEach(x => {
-
-      if(x.itemId == this.currentItem.itemId){
+      if (x.itemId == this.currentItem.itemId) {
         this.currentItem = x;
       }
-
     });
-    
+
+    this.imagePath = this.resourcePath + this.currentItem.itemId + ".png";
   }
 
-  nextItem(){
-
-
-    let idx: number  = 0
+  nextItem() {
+    let idx: number = 0
     this.shoppingList.forEach(x => {
 
-
-      if(x.itemId == this.currentItem.itemId){
-        if(idx+1 < this.shoppingList.length){
-          this.currentItem = this.shoppingList[idx+1];
+      if (x.itemId == this.currentItem.itemId) {
+        if (idx + 1 < this.shoppingList.length) {
+          this.currentItem = this.shoppingList[idx + 1];
           return;
         } else {
           console.log("No more next item.")
         }
       }
       idx++;
-      
     });
-
+    this.resourcePath = this.sessionService.getImageResourcePath() + "Maps/";
+    this.imagePath = this.resourcePath + this.currentItem.itemId + ".png";
   }
 
-  checkCurrentItem(){
+  checkCurrentItem() {
     this.currentItem.checked = true;
-
     this.sessionService.setShoppingList(this.shoppingList);
   }
 
-  prevItem(){
-
-    let idx: number  = 0
+  prevItem() {
+    let idx: number = 0
     this.shoppingList.forEach(x => {
 
-
-      if(x.itemId == this.currentItem.itemId){
-        if(idx != 0){
-          this.currentItem = this.shoppingList[idx-1];
+      if (x.itemId == this.currentItem.itemId) {
+        if (idx != 0) {
+          this.currentItem = this.shoppingList[idx - 1];
           return;
         } else {
           console.log("No more prev item.")
         }
       }
       idx++;
-      
     });
-    
-
+    this.resourcePath = this.sessionService.getImageResourcePath() + "Maps/";
+    this.imagePath = this.resourcePath + this.currentItem.itemId + ".png";
   }
 
   ngOnInit() {
     console.log("Init");
   }
 
-
-
-
-
-
   dismissModal() {
     this.modalController.dismiss({
       'dismissed': true
     });
   }
-
-
 }
